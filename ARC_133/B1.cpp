136ffa2761cd98f005628a4c173d7c9299d63524
r-koike <kmyrm0525@gmail.com>
2022-01-22 23:07:13 +0900

    ARC_133

---
+++ ARC_133/B1.cpp
@@ -0,0 +1,263 @@
+/**
+ * @brief  : c++ code for AtCoder
+ * @author : rk222
+ * @created: 2022.01.22 21:15:56
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define int long long
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> P;
+typedef pair<int, P> P1;
+typedef tuple<int, int, int> T;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<P> vp;
+typedef vector<tuple<int, int, int>> vt;
+
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+#define rep(i, x) for (int i = 0; i < x; i++)
+#define rep1(i, x) for (int i = 1; i <= x; i++)
+#define srep(i, s, x) for (int i = s; i < x; i++)
+#define rrep(i, x) for (int i = x - 1; i >= 0; i--)
+#define rrep1(i, x) for (int i = x; i > 0; i--)
+#define rsrep(i, s, x) for (int i = x - 1; i >= s; i--)
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+#define limit(x, l, r) max(l, min(x, r))
+#define lims(x, l, r) (x = max(l, min(x, r)))
+#define sor(v) sort(v.begin(), v.end())
+#define rev(s) reverse(s.begin(), s.end())
+#define lb(vec, a) lower_bound(vec.begin(), vec.end(), a)
+#define ub(vec, a) upper_bound(vec.begin(), vec.end(), a)
+#define uniq(vec) vec.erase(unique(vec.begin(), vec.end()), vec.end())
+#define mp1(a, b, c) P1(a, P(b, c))
+#define dame                                                                                  \
+    {                                                                                         \
+        puts("-1");                                                                           \
+        return 0;                                                                             \
+    }
+#define yn                                                                                    \
+    puts("Yes");                                                                              \
+    else puts("No")
+#define YN                                                                                    \
+    puts("YES");                                                                              \
+    else puts("NO")
+
+template <typename A, typename B> string to_string(pair<A, B> p);
+template <typename A, typename B, typename C> string to_string(tuple<A, B, C> p);
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+string to_string(const string &s) {
+    return '"' + s + '"';
+}
+string to_string(const char *s) {
+    return to_string((string)s);
+}
+string to_string(bool b) {
+    return (b ? "true" : "false");
+}
+string to_string(vector<bool> v) {
+    bool first = true;
+    string ret = "{";
+    for (int i = 0; i < static_cast<int>(v.size()); i++) {
+        if (!first) {
+            ret += ", ";
+        }
+        first = false;
+        ret += to_string(v[i]);
+    }
+    ret += "}";
+    return ret;
+}
+template <size_t N> string to_string(bitset<N> v) {
+    string ret = "";
+    for (size_t i = 0; i < N; i++) {
+        ret += static_cast<char>('0' + v[i]);
+    }
+    return ret;
+}
+template <typename A> string to_string(A v) {
+    bool first = true;
+    string ret = "{";
+    for (const auto &x : v) {
+        string s = to_string(x);
+        if (!first) {
+            ret += ", ";
+        }
+        if (s[0] == '{') {
+            s = "\n" + s;
+        }
+        first = false;
+        ret += s;
+    }
+    if (ret[ret.size() - 1] == '}') {
+        ret += "\n}";
+    } else {
+        ret += "}";
+    }
+    return ret;
+}
+template <typename A, typename... Lest> string to_string(A *v, int n, Lest... L) {
+    bool first = true;
+    string ret = "{";
+    rep(i, n) {
+        auto x = v[i];
+        string s = to_string(x, L...);
+        if (!first) {
+            ret += ", ";
+        }
+        if (s[0] == '{') {
+            s = "\n" + s;
+        }
+        first = false;
+        ret += s;
+    }
+    if (ret[ret.size() - 1] == '}') {
+        ret += "\n}";
+    } else {
+        ret += "}";
+    }
+    return ret;
+}
+template <typename A, typename B> string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+template <typename A, typename B, typename C> string to_string(tuple<A, B, C> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " +
+           to_string(get<2>(p)) + ")";
+}
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " +
+           to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+void name_disp(string s) {
+    cerr << "-------" << endl;
+    cerr << s << ": " << endl;
+}
+void val_disp() {
+    cerr << endl;
+}
+template <typename First, typename... Lest> void val_disp(First F, Lest... L) {
+    cerr << to_string(F) << endl;
+    val_disp(L...);
+}
+template <typename A, typename... Lest> void val_disp(A *v, Lest... L) {
+    cerr << to_string(v, L...) << endl;
+    val_disp();
+}
+#define disp(...)                                                                             \
+    name_disp(#__VA_ARGS__);                                                                  \
+    val_disp(__VA_ARGS__)
+#define adisp(a, ...)                                                                         \
+    name_disp(#a);                                                                            \
+    val_disp(a, __VA_ARGS__)
+
+#ifdef int
+const ll INF = 1002003004005006007ll;
+#else
+const int INF = 1002003004;
+#endif
+
+const int M = 100000000;
+const int dir_4[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+const int dir_8[8][2] = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+
+/* ------------------------------------- */
+
+ll n, p[200020], q[200020], n2q[200020], n2p[200020];
+vi vec[200020], vecj[200020];
+pair<P, P> dp[200020];
+signed main() {
+    scanf("%lld", &n);
+    rep(i, n) {
+        scanf("%lld", &p[i]);
+        // vec[i] = {};
+        ll x = p[i];
+        while (x <= n) {
+            // printf("%lld, %lld, %lld\n", i, x, n);
+            vec[i].pb(x);
+            x += p[i];
+        }
+        n2p[p[i]] = i;
+    }
+    rep(i, n) {
+        scanf("%lld", &q[i]);
+        ll x = q[i];
+        while (x <= n) {
+            // printf("%lld, %lld, %lld\n", i, x, n);
+            vecj[i].pb(x);
+            x += q[i];
+        }
+        n2q[q[i]] = i;
+    }
+
+    dp[0] = mp(mp(-1, -1), mp(-1, -1));
+    rep1(k, n) {
+        // i優先で小さくする
+        ll i0 = dp[k - 1].fr.fr;
+        ll j0 = dp[k - 1].fr.sc;
+        P pi = mp(INF, INF);
+        srep(i, i0 + 1, n) {
+            ll minj = INF;
+            rep(j, vec[i].size()) {
+                ll qid = n2q[vec[i][j]];
+                if (minj > qid && qid > j0) {
+                    minj = qid;
+                }
+            }
+            if (minj == INF) {
+                continue;
+            } else {
+                pi = mp(i, minj);
+                break;
+            }
+        }
+        // j優先で小さくする
+        ll i1 = dp[k - 1].sc.fr;
+        ll j1 = dp[k - 1].sc.sc;
+        P pj = mp(INF, INF);
+        srep(j, j1 + 1, n) {
+            ll mini = INF;
+            rep(i, vecj[j].size()) {
+                ll pid = n2p[vecj[j][i]];
+                if (mini > pid && pid > i1) {
+                    mini = pid;
+                }
+            }
+            if (mini == INF) {
+                continue;
+            } else {
+                pj = mp(j, mini);
+                break;
+            }
+        }
+
+        dp[k] = mp(pi, pj);
+    }
+
+    disp(dp, 10);
+
+    /* --------------------------------- */
+    return 0;
+};
